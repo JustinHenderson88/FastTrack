d6555d9ce49b427f9b661a0956782ab8c0089d96
Justin Henderson <jus10henderson@gmail.com>
2023-04-30 15:26:33 -0500

    Initial Commit

---
+++ FastTrack/FastTrackApp.swift
@@ -0,0 +1,17 @@
+//
+//  FastTrackApp.swift
+//  FastTrack
+//
+//  Created by Justin on 4/30/23.
+//
+
+import SwiftUI
+
+@main
+struct FastTrackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
